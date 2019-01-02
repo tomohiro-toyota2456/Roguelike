052b155f68e6c99d167e70efddc0723456ecdc9d
tomohiro-toyota2456 <toyota.6542@gmail.com>
2019-01-02 17:08:13 +0900

    ダンジョン自動生成＋α

---
+++ Assets/Resources/Script/MainGameScene/LabyrinthSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ダンジョン生成用設定クラス
+/// </summary>
+[SerializeField]
+public class LabyrinthSettings : MonoBehaviour {
+
+    [Header(""),Range(2, 10)]
+    public int minWidth;
+    [Header(""), Range(2, 10)]
+    public int minHeight;
+    [Header(""), Range(0, 100)]
+    public int roomRate;
+    [Header(""), Range(1, 10)]
+    public int maxWallThickness;
+}
---
+++ Assets/Resources/Script/MainGameScene/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room {
+    public readonly int x;
+    public readonly int y;
+    public readonly int width;
+    public readonly int height;
+
+    public Room(int x,int y, int width,int height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+}
